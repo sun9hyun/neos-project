db80c37a3726cedd97e53b4ea2da88bf00658581
JongwooParks <mwoaoint0124@gamil.com>
2022-11-13 10:14:00 +0900

    박종우 git 기초 구성 controller, static, templates폴더 패키징 fragmentTest.html 예제 파일 배포 컨트롤러 연결

---
+++ src/main/java/com/example/neosproject/controller/fix/FixController.java
@@ -0,0 +1,14 @@
+package com.example.neosproject.controller.fix;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/fix/*")
+public class FixController {
+    @GetMapping("/header")
+    public String fix(){
+        return "app/fix/header";
+    }
+}

17caeae67dd1d01f61b65374125e2883be3af605
shakecoding <hogeun4656@gmail.com>
2022-11-14 09:07:22 +0900

    11월 14일 네오스인 로그인 전 후 따로 있습니다

--- src/main/java/com/example/neosproject/controller/neosUser/NeosUserController.java
+++ src/main/java/com/example/neosproject/controller/neosUser/NeosUserController.java
@@ -1,4 +1,26 @@
 package com.example.neosproject.controller.neosUser;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/neosUser/*")
 public class NeosUserController {
+
+    @GetMapping("/listBefore")
+    public String listBefore(){
+        return "app/neosUser/neosListBefore";
+    }
+
+    @GetMapping("/listAfter")
+    public String listAfter(){
+        return "app/neosUser/neosListAfter";
+    }
+
+    @GetMapping("/info")
+    public String info(){
+        return "app/neosUser/userInfo";
+    }
+
 }
---
+++ src/main/resources/static/js/neosUser/neosList.js
@@ -0,0 +1,9 @@
+$(".userName").on("mouseover", function () {
+    // $(this).css("color", "#24A");
+    $(this).children('.idHoverMenu').css("display", "block");
+})
+
+$(".userName").on("mouseout", function () {
+    // $(this).css("color", "#2A4");
+    $(this).children('.idHoverMenu').css("display", "none");
+})

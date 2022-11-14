a391a9ebfaf52799fac7f037e65f1b66b7dbbeb2
simminsoung <tlaalstjd15@naver.com>
2022-11-15 01:04:14 +0900

    심민성 공지사항 / 공지사항 상세보기 / 문의하기 / 문의하기 댓글 / 로그인 , 회원가입 모달 퍼블리싱

---
+++ src/main/resources/static/js/loginAndJoin/join.js
@@ -0,0 +1,8 @@
+// $( document ).ready(function() {
+//     console.log("들어왔다");
+//     $('.joinAndLogin').on('click', function() {
+//     $('.loginModal').toggleClass('open');
+//     return false;
+// });
+// });
+

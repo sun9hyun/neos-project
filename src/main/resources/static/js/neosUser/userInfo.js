4c82253c4da70ae744acb81a210102b989d7adde
shakecoding <hogeun4656@gmail.com>
2022-11-15 00:50:26 +0900

    로그인 전 후 따로 네오스인, 프로필 상세보기 자신과 타인 구분, 본인인증 여부 확인 추가

--- src/main/resources/static/js/neosUser/userInfo.js
+++ src/main/resources/static/js/neosUser/userInfo.js
@@ -26,6 +26,68 @@ $(".favorite").on("click", function () {
     $(this).toggleClass('active');
 })
 
+// $(".top .favorite2").on("click", function () {
+//     $(".modalWrapOpen").css("display", "block");
+// })
+//
+// $(".modalWrapOpen .commonModal .btnWrap button.whiteBtn").on("click", function () {
+//     $(".modalWrapOpen").css("display", "none");
+// })
+//
+// $(".modalWrapOpen .commonModal .closeBtn img").on("click", function () {
+//     $(".modalWrapOpen").css("display", "none");
+// })
+//
+// $(".modalWrapOpen .commonModal .btnWrap button.redBtn").on("click", function () {
+//     $(".modalWrapOpen").css("display", "none");
+//     $(".top .favorite2").addClass('active2');
+//     $(".top .favorite2").removeClass('favorite2');
+// })
+//
+// $(".top .active2").on("click", function () {
+//     $(".top active2").addClass('favorite2');
+//     $(".top .favorite2").removeClass('active2');
+// })
+
+
+
+$(".favorite2").click(function(){
+    if($(this).hasClass("off")) {
+        $(".modalWrapOpen").css("display", "block");
+    } else {
+        $(this).removeClass('active2');
+        $(this).addClass('off');
+        $(this).removeClass('on');
+    }
+});
+
+// $(".favorite2").on("click", function () {
+//     $(".modalWrapOpen").css("display", "block");
+// })
+
+$(".modalWrapOpen .commonModal .btnWrap button.whiteBtn").on("click", function () {
+    $(".modalWrapOpen").css("display", "none");
+})
+
+$(".modalWrapOpen .commonModal .closeBtn img").on("click", function () {
+    $(".modalWrapOpen").css("display", "none");
+})
+
+$(".modalWrapOpen .commonModal .btnWrap button.redBtn").on("click", function () {
+    $(".modalWrapOpen").css("display", "none");
+    $(".top .favorite2").addClass('active2');
+    $(".top .favorite2").addClass('on');
+    $(".top .favorite2").removeClass('off');
+})
+
+
+
+
+
+
+
+
+
 
 
 

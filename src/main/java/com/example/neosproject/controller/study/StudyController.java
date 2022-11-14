075229864a902a5886d249056b1803c73e294fe2
redhawk90 <slions90@naver.com>
2022-11-14 09:10:29 +0900

    추성윤 스터디 목록, 스터디 작성, 상세보기 일부

--- src/main/java/com/example/neosproject/controller/study/StudyController.java
+++ src/main/java/com/example/neosproject/controller/study/StudyController.java
@@ -1,4 +1,31 @@
 package com.example.neosproject.controller.study;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/study/*")
 public class StudyController {
+
+    @GetMapping("/create")
+    public String create(){
+        return "app/study/createStudy";
+    }
+
+    @GetMapping("/list")
+    public String list(){
+        return "app/study/listStudy";
+    }
+
+    @GetMapping("/detail")
+    public String detail(){
+        return "app/study/detailStudy";
+    }
+
+    @GetMapping("/question")
+    public String question(){
+        return "app/study/questionStudy";
+    }
+
 }

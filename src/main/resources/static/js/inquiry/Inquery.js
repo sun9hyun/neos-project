8d5fbc7f47783adf6f86907137b3abe57b14b59f
simminsoung <tlaalstjd15@naver.com>
2022-11-17 17:33:03 +0900

    심민성  / 문의하기 , 문의하기 답변  textarea  수정 완료

---
+++ src/main/resources/static/js/inquiry/Inquery.js
@@ -0,0 +1,20 @@
+// $(".communiWrap").on("keyup", ".replyTextArea",function (e) {
+//         let content = $(this).val();
+//         if (content.length == 0 || content == "") {
+//             $(this).parent().next().find(".replyTextCount").text("0");
+//         } else {
+//             $(this).parent().next().find(".replyTextCount").text(content.length);
+//         }
+//     }
+// );
+//
+//
+// $(".replyComponent_reply__3l-Wc").on("keyup", ".replyTextArea",function (e) {
+//         let content = $(this).val();
+//         if (content.length == 0 || content == "") {
+//             $(this).parent().next().find(".replyTextCount").text("0");
+//         } else {
+//             $(this).parent().next().find(".replyTextCount").text(content.length);
+//         }
+//     }
+// );

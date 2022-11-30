2a0943f6cc43a2c0c35be442818b5f80112c6b54
JongwooParks <mwoaoint0124@gmail.com>
2022-11-30 17:45:36 +0900

    박종우 회원가입 대학교 선택가능 이메일 인증 가능 로그인 처리 카카오톡 구현 90% 완료

---
+++ src/main/java/com/app/neos/controller/join/EmailController.java
@@ -0,0 +1,46 @@
+package com.app.neos.controller.join;
+
+
+import com.app.neos.service.join.EmailService;
+import com.app.neos.service.join.JoinService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/send/*")
+@Slf4j
+@RequiredArgsConstructor
+public class EmailController {
+    private final EmailService emailService;
+    private final JoinService joinService;
+
+    @GetMapping("/emailConfirm")
+    public String emailConfirm(@RequestParam String email) throws Exception {
+//            log.info(email);
+        String confirm = emailService.sendSimpleMessage(email, null);
+
+        return "app/main/TestMain";
+    }
+
+    @GetMapping("/email")
+    public RedirectView email(String token, String email) throws Exception{
+        emailService.sendSimpleMessage(email, token);
+        return new RedirectView("/main/main?join=need");
+    }
+
+
+
+    @GetMapping("/ok")
+    public RedirectView emailOk(@RequestParam String token){
+        log.info("==================================================");
+        log.info(token);
+        joinService.certify(token);
+        return new RedirectView("/main/main?certify=true");
+    }
+}

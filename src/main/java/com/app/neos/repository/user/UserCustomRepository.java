d3bc506dc2aa8b052743316bf9aba9ee9e7fb485
simminsoung <tlaalstjd15@naver.com>
2022-12-01 17:29:41 +0900

    심민성 문의하기 공지사항 CSS 수정 / 네오스인 조회 50% 완료

--- src/main/java/com/app/neos/repository/user/UserCustomRepository.java
+++ src/main/java/com/app/neos/repository/user/UserCustomRepository.java
@@ -23,4 +23,5 @@ public interface UserCustomRepository {
 
     public UserDTO findById(Long userId);
 
+
 }
--- src/main/java/com/app/neos/service/neosUser/NeosUserService.java
+++ src/main/java/com/app/neos/service/neosUser/NeosUserService.java
@@ -1,4 +1,29 @@
 package com.app.neos.service.neosUser;
 
+import com.app.neos.domain.user.UserDTO;
+import com.app.neos.entity.user.User;
+import com.app.neos.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class NeosUserService {
+
+    private final UserRepository userRepository;
+
+    // 유저 추가
+    public void saveUser(User user){
+        userRepository.save(user);
+    }
+
+    // 유저 목록
+    public List<User> findUser() {
+        return userRepository.findAll();
+    }
+
 }

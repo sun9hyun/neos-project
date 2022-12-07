d1c60c214aafcd0a9351ad0853ee3ff7efa781cd
simminsoung <tlaalstjd15@naver.com>
2022-12-07 17:20:00 +0900

    [심민성] 유저 상세보기 유저가 활동하는 스터디 완료 / 로그인 헤더 네오스인 -> 모집하기 변경 , 헤더 모집하기 옆 손가락 아이콘 애니메이션 효과 완료 , (구)네오스인  (현)모집하기 퍼블리싱 완료

--- src/main/java/com/app/neos/repository/follow/FollowRepository.java
+++ src/main/java/com/app/neos/repository/follow/FollowRepository.java
@@ -4,4 +4,6 @@ import com.app.neos.entity.follow.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+
 }
